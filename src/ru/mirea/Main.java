84594623584916c12a95abe439fd321e56df07c9
Dude <BAX.152MM@yandex.ru>
2020-10-05 23:09:08 +0300

    Final#910

--- src/ru/mirea/Main.java
+++ src/ru/mirea/Main.java
@@ -1,11 +1,10 @@
 package ru.mirea;
 
-import ru.mirea.Prakt8.exercise1.FurnitureShop;
+import ru.mirea.Prakt910.Company;
 
 public class Main {
 
     public static void main(String[] args) {
-        FurnitureShop maen = new FurnitureShop();
-        maen.main();
+        Company company = new Company();
     }
 }
---
+++ src/ru/mirea/Prakt910/EmployeePosition.java
@@ -0,0 +1,6 @@
+package ru.mirea.Prakt910;
+
+public interface EmployeePosition {
+    public String getJobTitle();
+    double calcSalary(double baseSalary);
+}
