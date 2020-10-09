927000c4102c11f886950dcfbba2d909070dc0b9
Duder <BAX.152MM@yandex.ru>
2020-10-09 11:39:12 +0300

    Dop#15

--- src/ru/mirea/Prakt1112/exercise2/Calculator.java
+++ src/ru/mirea/Prakt1112/exercise2/Calculator.java
@@ -1,14 +1,21 @@
 package ru.mirea.Prakt1112.exercise2;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
+import javafx.geometry.Orientation;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 
 public class Calculator extends Application {
 
@@ -17,20 +24,20 @@ public class Calculator extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        Text text = new Text("Hello from JavaFX!");
-        text.setLayoutY(80);    // установка положения надписи по оси Y
-        text.setLayoutX(100);   // установка положения надписи по оси X
-
-        Group group = new Group(text);
-
-        Scene scene = new Scene(group);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Калькулятор");
-        primaryStage.setWidth(300);
-        primaryStage.setHeight(250);
+    public void start(Stage primaryStage) throws IOException {
 
         primaryStage.getIcons().add(new Image(Calculator.class.getResourceAsStream( "ico.png" )));
+
+        FXMLLoader loader = new FXMLLoader();
+        URL xmlUrl = getClass().getResource("calc.fxml");
+        loader.setLocation(xmlUrl);
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Калькулятор");
+        primaryStage.setWidth(160);
+        primaryStage.setHeight(280);
 
         primaryStage.show();
     }
