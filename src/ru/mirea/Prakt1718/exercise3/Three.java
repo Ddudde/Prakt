650bc5657f42e26c0359e6942f7541d18faa24a8
Ddudde <BAX.152MM@yandex.ru>
2020-10-30 11:06:43 +0300

    Dop#24

--- src/ru/mirea/Prakt1718/exercise3/Three.java
+++ src/ru/mirea/Prakt1718/exercise3/Three.java
@@ -6,10 +6,10 @@ import java.util.regex.Pattern;
 public class Three {
     public static void main(String[] args) {
         System.out.println("Список цен:");
-        String inp = "Hell\nWindows";
+        String inp = "1000.10 USD\nWindows";
         System.out.println(inp);
         System.out.println();
-        Pattern pattern = Pattern.compile("Windows",Pattern.MULTILINE);
+        Pattern pattern = Pattern.compile("^[0-9]*.[0-9]{2} (USD|RUR|EU)$",Pattern.MULTILINE);
         Matcher matcher = pattern.matcher(inp);
         System.out.println("Список верифицированных цен:");
         System.out.println(matcher.find() ? matcher.group() : "Не найдено ценников");
