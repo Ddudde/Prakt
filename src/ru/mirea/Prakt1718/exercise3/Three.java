60dee329dba6a2a93edb6dfa4057e2cc491dbff9
Ddudde <BAX.152MM@yandex.ru>
2020-10-30 11:44:44 +0300

    Dop#25

--- src/ru/mirea/Prakt1718/exercise3/Three.java
+++ src/ru/mirea/Prakt1718/exercise3/Three.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class Three {
     public static void main(String[] args) {
         System.out.println("Список цен:");
-        String[] inp = new String[]{"23.78 USD", "22 UDD", "0.002 USD", "139.90 RUR", "99.90 RUR"};
+        String[] inp = new String[]{"23.78 USD", "22 UDD", "0.002 USD", "139.90 RUR", "99.90 RUR", "99 RUR"};
         for (String f : inp)
             System.out.println(f);
         System.out.println();
