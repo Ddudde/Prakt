b0adae7d0c614aa6789602944f7f225e1deea8e9
Ddudde <BAX.152MM@yandex.ru>
2020-11-06 11:13:11 +0300

    Final#1920

--- src/ru/mirea/Main.java
+++ src/ru/mirea/Main.java
@@ -1,11 +1,10 @@
 package ru.mirea;
 
-import ru.mirea.Prakt1112.exercise1.Hello;
-import ru.mirea.Prakt910.Company;
+import ru.mirea.Prakt1920.Generator;
 
 public class Main {
 
     public static void main(String[] args) {
-        Hello hello = new Hello();
+        new Generator();
     }
 }
