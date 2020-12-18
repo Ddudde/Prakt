5c8af9d4677663006ebf1fc2ed3b23a79e61265e
Ddudde <BAX.152MM@yandex.ru>
2020-12-19 00:11:34 +0300

    Final#3132

---
+++ src/ru/mirea/Prakt3132/src/test/java/ru/mirea/Prakt3132/Prakt3132ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.Prakt3132;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prakt3132ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
